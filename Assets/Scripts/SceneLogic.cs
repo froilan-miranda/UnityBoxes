c073810d91b134e3683183bb6fdb397578f3c516
Froilan Miranda <froilan.miranda@gmail.com>
2015-10-01 22:19:49 -0400

    added current unity files

---
+++ Assets/Scripts/SceneLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLogic : MonoBehaviour {
+
+	private GameObject tQuad;
+	private Mesh tMesh;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("we're back");
+
+
+		tQuad = GameObject.Find("testMesh");
+		tMesh = tQuad.GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = tMesh.vertices;
+		int i = 0;
+		while(i<vertices.Length){
+			vertices[i] += Vector3.up * Time.deltaTime;
+			i++;
+		}
+		tMesh.vertices = vertices;
+		tMesh.RecalculateBounds();
+
+		foreach(Vector3 vert in vertices){
+			Debug.Log(transform.TransformPoint(vert));
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+
+ private float amplitudeX = 10.0f;
+ private float amplitudeY = 5.0f;
+ private float omegaX = 1.0f;
+ private float omegaY = 5.0f;
+ private float index;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+ void Update () {
+     index += Time.deltaTime;
+     float x = amplitudeX*Mathf.Cos (omegaX*index);
+     float y = Mathf.Abs (amplitudeY*Mathf.Sin (omegaY*index));
+     transform.localPosition= new Vector3(x,y,0);
+ }
+}
